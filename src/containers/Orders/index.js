ff4e5f688e14d5aa163f1ba5dddb9e29ca673f38
marcioramires <marcioramiresgt@gmail.com>
2022-12-12 21:05:29 -0300

    order card updated

--- src/containers/Orders/index.js
+++ src/containers/Orders/index.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import Button from "../../components/Button";
 import Image from "../../components/Img";
-import OrderCard from "../../components/OrderCard";
 import Title from "../../components/Title";
 
-import { Wrapped } from "./styles";
+import { OrderCard, Wrapped } from "./styles";
 
 import Burger2 from "../../assets/burger2.svg";
+import Trash from "../../assets/trash.svg";
 
 const App = () => {
 
@@ -16,7 +16,11 @@ const App = () => {
             <Image src={Burger2} />
             <Title>Pedidos</Title>
             <OrderCard>
-
+                <div>
+                    <h2>1 Coca-cola, 1 Xis-salada</h2>
+                    <p>Marcio Tambeiro</p>
+                </div>
+                <button><img alt="lata-de-lixo" src={Trash} /></button>
             </OrderCard>
             <Button isBack>Voltar</Button>
         </Wrapped>
--- src/containers/Orders/styles.js
+++ src/containers/Orders/styles.js
@@ -8,3 +8,57 @@ export const Wrapped = styled.div`
     height: 100vh;
     width: 100vw;
 `
+
+export const OrderCard = styled.div`
+    width: 342px;
+    height: 101px;
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 14px;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    margin-top: 50px;
+
+    div {
+        display: flex;
+        flex-direction: column;
+        background: none;
+
+        h2 {
+            font-weight: 300;
+            font-size: 18px;
+            line-height: 21px;
+            color: #FFFFFF;
+            background: none;
+            margin: 17px 0px 0px 27px;
+        }
+
+        p {
+            font-weight: 700;
+            font-size: 18px;
+            line-height: 21px;
+            color: #FFFFFF;
+            background: none;
+            margin: 24px 0px 0px 27px;
+        }
+    }
+
+    button {
+        background: none;
+        border: none;
+        cursor: pointer;
+
+        img {
+            background: none;
+            margin-right: 15px;
+        }
+        
+        :hover {
+            opacity: 0.7;
+        }
+
+        :active {
+           opacity: 0.4
+        }
+    }
+`
